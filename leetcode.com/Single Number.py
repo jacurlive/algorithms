2d98fc3cebc7512b22be367f7c689795aa9935d7
kkirchh <jasurkkirchh@gmail.com>
2022-11-05 14:34:57 -0500

    update

--- leetcode.com/Single Number.py
+++ leetcode.com/Single Number.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/single-number/
 
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
