2486bc03c661c9f662706678867e7bba402005c1
kkirchh <jasurkkirchh@gmail.com>
2022-11-05 14:23:36 -0500

    update

--- leetcode.com/Guess Number Higher or Lower.py
+++ leetcode.com/Guess Number Higher or Lower.py
@@ -1,4 +1,13 @@
 # https://leetcode.com/problems/guess-number-higher-or-lower/
 
-def guess_number(num: int):
-    pass
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 0, n
+        while l <= r:
+            mid = (l + r) >> 1
+            if guess(mid) == -1:
+                r = mid - 1
+            elif guess(mid) == 1:
+                l = mid + 1
+            elif guess(mid) == 0:
+                return mid
